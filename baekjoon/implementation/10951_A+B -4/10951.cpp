85c8f57dbfab50671a680f4e1522c70bb9c25657
kdjun97 <kdjun97@gmail.com>
2022-06-12 13:55:37 +0900

    Docs: Add baekjoon 10951

---
+++ baekjoon/implementation/10951_A+B -4/10951.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int a, b;
+    while (!(cin >> a >> b).eof()) {
+        cout << a+b << "\n";
+    }
+    return 0;
+}
